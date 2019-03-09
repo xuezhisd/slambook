36508d775650231d9218b43964d9c602a6ac7e7c
xuezhi.zhang <xuezhi.zhang@horizon.ai>
2019-03-09 23:26:40 +0800

    edit ch2 & ch3

---
+++ ch3/useEigen/eigenMatrix2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <ctime>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#define MATRIX_SIZE 50
+
+
+int main(int argc, char** argv) {
+
+	Eigen::Matrix<float, 2, 3> matrix_23;
+
+	Eigen::Vector3d v_3d;
+
+	Eigen::Matrix<float, 3, 1> vd_3d;
+
+
+
+
+
+}
